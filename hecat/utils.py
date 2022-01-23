4169f54ce2699355b79663e76bb2e79e4ca03d82
nodiscc <nodiscc@gmail.com>
2022-01-23 16:52:59 +0100

    utils: to_kebab_case: remove &/, characters

--- hecat/utils.py
+++ hecat/utils.py
@@ -15,7 +15,9 @@ def to_kebab_case(string):
         ' ': '-',
         '(': '',
         ')': '',
-        '&': ''
+        '&': '',
+        '/': '',
+        ',': ''
     }
     newstring = string.translate(str.maketrans(replacements)).lower()
     return newstring
