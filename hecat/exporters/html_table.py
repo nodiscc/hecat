dd3e9813c88af45d22ee11b3df4644a6c3f61bf9
nodiscc <nodiscc@gmail.com>
2023-03-13 18:35:06 +0100

    exporters: html_table: fix tag search

--- hecat/exporters/html_table.py
+++ hecat/exporters/html_table.py
@@ -121,7 +121,7 @@ function myFunctionTag() {
 
   // Loop through all table rows, and hide those who don't match the search query
   for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[1];
+    td = tr[i].getElementsByTagName("td")[2];
     if (td) {
       txtValue = td.textContent || td.innerText;
       if (txtValue.toUpperCase().indexOf(filter) > -1) {
